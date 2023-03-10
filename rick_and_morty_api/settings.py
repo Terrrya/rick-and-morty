88bfea6a4d110e4af91ac89e3b244425f2dbd517
Denys Tereshchuk <terrya@ukr.net>
2023-03-10 23:50:53 +0400

    Fixed line length and quotes in settings

--- rick_and_morty_api/settings.py
+++ rick_and_morty_api/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7y0n0%%50g0a4%40q8%r_5rb00h4m0ac=sqw=8oe7xsb(xu^gx"
+SECRET_KEY = (
+
+    "django-insecure-7y0n0%%50g0a4%40q8%r_5rb00h4m0ac=sqw=8oe7xsb(xu^gx"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,8 +57,7 @@ ROOT_URLCONF = "rick_and_morty_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
